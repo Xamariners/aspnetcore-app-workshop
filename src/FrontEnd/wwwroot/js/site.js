982d2dd4c2785f9ef3407c9d49b29d269250d337
Ben Ishiyama-Levy <ben@xamariners.com>
2018-09-03 16:50:20 +0800

    added map

--- src/FrontEnd/wwwroot/js/site.js
+++ src/FrontEnd/wwwroot/js/site.js
@@ -18,9 +18,15 @@ function addRequestPins(bingMap, requestData) {
     var locations = [];
     $.each(requestData, function (index, data) {
         var location = new Microsoft.Maps.Location(data.lat, data.long);
+
         locations.push(location);
-        var order = index + 1;
-        var pin = new Microsoft.Maps.Pushpin(location, { title: data.name, color: data.color, text: order.toString() });
+
+        var pin = new Microsoft.Maps.Pushpin(location, { title: data.name, color: data.color });
+        
+        Microsoft.Maps.Events.addHandler(pin, 'click', function () {
+            window.location.href = data.url;
+        });
+
         bingMap.entities.push(pin);
     });
     var rect = Microsoft.Maps.LocationRect.fromLocations(locations);
